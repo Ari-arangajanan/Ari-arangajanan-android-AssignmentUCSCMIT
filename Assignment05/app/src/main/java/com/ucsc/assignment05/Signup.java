fd0f22d6b3d7ed3137de5346829e74f6924e31f4
Ari arangajanan <arangajanan.nar@gmail.com>
2024-07-16 19:04:17 +0530

    signup change

--- Assignment05/app/src/main/java/com/ucsc/assignment05/Signup.java
+++ Assignment05/app/src/main/java/com/ucsc/assignment05/Signup.java
@@ -1,6 +1,95 @@
 package com.ucsc.assignment05;
-
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends Activity {
+    private EditText userName, userEmail, userPassword;
+    private Button regButton;
+    private TextView userLogin;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+        setupUIViews();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validate())
+                {
+                    //upload to database
+                    String user_email=userEmail.getText().toString().trim();
+                    String user_password=userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email,user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Signup.this, "Registration successfull", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Signup.this, Home.class));
+                            } else {
+                                Toast.makeText(Signup.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this, MainActivity.class));
+            }
+        });
+    }
+
+    private Boolean validate(){
+        boolean result = false;
+
+        String name = userName.getText().toString();
+        String password = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+
+        if(name.isEmpty() || password.isEmpty() ||  email.isEmpty())
+        {
+            Toast.makeText(Signup.this, "Please enter all the details",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            result = true;
+        }
+        return result;
+    }
+
+    private void setupUIViews(){
+        userName=(EditText)findViewById(R.id.InputNewUserName);
+        userPassword=(EditText)findViewById(R.id.InputNewPassword);
+        regButton=(Button)findViewById(R.id.register_button);
+        userLogin=(TextView)findViewById(R.id.login_button);
+    }
 }
